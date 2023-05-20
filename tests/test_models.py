f26ce3864344accefb31cedb2684ca2bac0898a7
Jay Cho <jaycho1214@gmail.com>
2023-05-20 18:44:17 +0900

    prettified by black

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,46 +1,21 @@
-import unittest
-
 from osori_scraper.models.song import Song
 
+
 def test_song_eq():
-  assert Song(
-    id=1, 
-    name='test', 
-    composer='test',
-    highest_note='test', 
-    lowest_note='test'
+    assert Song(
+        id=1, name="test", composer="test", highest_note="test", lowest_note="test"
     ) == Song(
-    id=1,
-    name='test1', 
-    composer='test2',
-    highest_note='test3', 
-    lowest_note='test4'
-    ), 'Song class should be treated equal if it has the same id'
+        id=1, name="test1", composer="test2", highest_note="test3", lowest_note="test4"
+    ), "Song class should be treated equal if it has the same id"
 
-  assert Song(
-    id=1, 
-    name='test', 
-    composer='test',
-    highest_note='test', 
-    lowest_note='test'
+    assert Song(
+        id=1, name="test", composer="test", highest_note="test", lowest_note="test"
     ) == Song(
-    id=1,
-    name='test', 
-    composer='test',
-    highest_note='test', 
-    lowest_note='test'
-    ), 'Song class are not equal'
+        id=1, name="test", composer="test", highest_note="test", lowest_note="test"
+    ), "Song class are not equal"
 
-  assert Song(
-    id=2, 
-    name='test', 
-    composer='test',
-    highest_note='test', 
-    lowest_note='test'
+    assert Song(
+        id=2, name="test", composer="test", highest_note="test", lowest_note="test"
     ) == Song(
-    id=1,
-    name='test', 
-    composer='test',
-    highest_note='test', 
-    lowest_note='test'
-    ), 'Song class should not be treated equal if it has the different id'
+        id=1, name="test", composer="test", highest_note="test", lowest_note="test"
+    ), "Song class should not be treated equal if it has the different id"
