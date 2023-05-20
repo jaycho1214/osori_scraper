2987defbaa1f524d55202e0f1da0c563bad2f69e
Jay Cho <jaycho1214@gmail.com>
2023-05-20 19:35:58 +0900

    add new error class: DuplicatedValue

--- osori_scraper/core/exceptions.py
+++ osori_scraper/core/exceptions.py
@@ -15,3 +15,11 @@ class MissingFieldValue(Exception):
     def __init__(self, field: str) -> None:
         self.message = f"{field}이 json 파일에서 발견되지 않음"
         super().__init__(self.message)
+
+
+class DuplicatedValue(Exception):
+    """JSON파일에서 중복된 값이 있을때"""
+
+    def __init__(self, index1: int, index2: int) -> None:
+        self.message = f"`{index1}`와 `{index2}`에서 중복된 값 발견"
+        super().__init__(self.message)
