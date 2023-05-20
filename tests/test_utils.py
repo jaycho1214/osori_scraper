fa117c154e4759529f8c839422c5b458c1c330e5
Jay Cho <jaycho1214@gmail.com>
2023-05-20 21:47:46 +0900

    add sample json file and test for json parser

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,10 +1,17 @@
-import unittest
+import pytest
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+SAMPLES_DIR = Path(__file__).resolve().parent / "samples"
 
+from osori_scraper.models.song import Song
 from osori_scraper.utils.json import JSONParser
+from osori_scraper.core.exceptions import *
 
 
 def test_check_json():
-    pass
+    parser = JSONParser(SAMPLES_DIR / "invalid_song_data1.json", Song)
+    with pytest.raises(MissingFieldValue):
+        parser.validate()
+    parser = JSONParser(SAMPLES_DIR / "invalid_song_data2.json", Song)
+    with pytest.raises(DuplicatedValue):
+        parser.validate()
